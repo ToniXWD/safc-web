4b2af9a1f55c0b9d4958c5012684bee62b9749db
ToniXWD <807077266@qq.com>
2024-11-07 00:15:40 +0800

    修改bug

--- src/components/GoogleSignIn.tsx
+++ src/components/GoogleSignIn.tsx
@@ -2,15 +2,36 @@
 
 import React from "react";
 
+interface GoogleBasicProfile {
+    getId(): string;
+    getName(): string;
+    getEmail(): string;
+    getImageUrl(): string;
+    getGivenName(): string;
+    getFamilyName(): string;
+}
+
+interface GoogleUser {
+    getBasicProfile(): GoogleBasicProfile;
+    getAuthResponse(): {
+        id_token: string;
+        scope: string;
+        expires_in: number;
+        access_token: string;
+        first_issued_at: number;
+        expires_at: number;
+    };
+}
+
 declare global {
     interface Window {
-        onSignIn: (googleUser: any) => void;
+        onSignIn: (googleUser: GoogleUser) => void;
     }
 }
 
 export function GoogleSignIn() {
     React.useEffect(() => {
-        window.onSignIn = (googleUser) => {
+        window.onSignIn = (googleUser: GoogleUser) => {
             const profile = googleUser.getBasicProfile();
             console.log('ID: ' + profile.getId());
             console.log('Name: ' + profile.getName());
